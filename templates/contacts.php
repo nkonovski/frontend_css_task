7b606f3d6096ccfcbdc34df85d8659b16d097e50
nkonovski <nkonovski@gmail.com>
2016-03-05 17:38:59 +0200

    finish about and contact pages

--- templates/contacts.php
+++ templates/contacts.php
@@ -1,1 +1,10 @@
-<div id="map-canvas" style="height: 300px; width: 900px;"></div>
+<h1 class="">Contact Us. Locate Us. Send Us Your Thoughts!</h1>
+<p class="subtitle">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent mauris. Maecenas in magna mollis lectus lacinia mollis.</p>
+<div id="map-canvas" style="height: 400px; width: 940px;"></div>
+<form class="contact-form">
+<input type="text" name="firstname" placeholder="FIRST NAME" />
+<input type="text" name="lastname" placeholder="LAST NAME" />
+<input type="text" name="email" placeholder="EMAIL" />
+<textarea placeholder="MESSAGE"></textarea>
+<input type="submit" value="">
+</form>
